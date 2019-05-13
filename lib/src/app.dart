aa20c40780e106c1654b1a252c744f7460b06767
Leonid Emelaynov <Emelyanov.L.Dm@sberbank.ru>
2019-05-13 13:08:03 +0300

    Adding dark theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mphm_mobile/src/blocs/login_bloc.dart';
+import 'package:mphm_mobile/src/themes.dart';
 import 'package:mphm_mobile/src/ui/home_page.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ChangeNotifierProvider<LoginBloc>(
-          builder: (_) => LoginBloc(),
-          child: HomePage(),
-        ),
-      );
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.light,
+    ));
+
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        accentColor: AppColors.accentColor,
+        canvasColor: Colors.transparent,
+      ),
+      home: ChangeNotifierProvider<LoginBloc>(
+        builder: (_) => LoginBloc(),
+        child: HomePage(),
+      ),
+    );
+  }
 }
---
+++ lib/src/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final backgroundColor = Color(0xFF252d3b);
+  static final accentColor = Color(0xFF25ce77);
+  static final bottomPanelBackgroundColor = Color(0xFF3d4655);
+  static final bottomDialogStartColor = Color(0xFF646f82);
+  static final bottomDialogEndColor = Color(0xFF252d3c);
+}
